b22d13daea28e7aea022ec13481d25643f1eb8fd
Julien Truffaut <truffaut.julien@gmail.com>
2015-01-14 12:46:14 +0100

    Add chapter 3 exo

--- exercice/src/main/scala/fp/Chapter2.scala
+++ exercice/src/main/scala/fp/Chapter2.scala
@@ -13,15 +13,15 @@ object Chapter2 {
 
 
   // 2.3
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) = ???
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
 
 
   // 2.4
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = ???
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a,b) => f(a)(b)
 
 
   // 2.5
-  def compose[A,B,C](f: B => C, g: A => B): A => C = ???
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 
 
 
--- exercice/src/main/scala/fp/Chapter3.scala
+++ exercice/src/main/scala/fp/Chapter3.scala
@@ -46,6 +46,39 @@ object Chapter3 {
     // 3.5
     def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 
+    // 3.6
+    def init[A](l: List[A]): List[A] = ???
+
+    def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+    def sum2(ns: List[Int]) = foldRight(ns, 0)((x,y) => x + y)
+    def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+    // 3.7 can we implement using foldRight? Can we stop iteration if we encounter 0.0
+
+    // 3.8 what is the result of... What is the relationship between foldRight and Cons.apply
+    foldRight(List(1,2,3), Nil: List[Int])(Cons(_, _))
+
+    // 3.9 length in term of foldRight
+    def length[A](as: List[A]): Int = ???
+
+    // 3.10 verify that foldRight is not tailrec, implement tailrec foldLeft
+    def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = ???
+
+    // 3.11 write sum, product, and a function to compute the length of a list using foldLeft.
+
+    // 3.12 write reverse using fold
+
+    // 3.13 write foldLeft in terms of foldRight, can we do in the other way around?
+
+    // 3.14 Implement append in terms of either foldLeft or foldRight.
+
+    // 3.15 Hard: Write a function that concatenates a list of lists into a single list.
+    // Its runtime should be linear in the total length of all lists. Try to use functions we have already defined.
+
 
   }
 
