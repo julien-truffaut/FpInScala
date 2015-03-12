809e1e0cacdcbf91c778f574dc49af5aad1304fa
Gerard Basler <gerard.basler@leonteq.com>
2015-03-12 12:24:59 +0100

    Implemented chapter 6.4.

--- response/src/main/scala/gerard/Chapter6.scala
+++ response/src/main/scala/gerard/Chapter6.scala
@@ -22,23 +22,48 @@ object Chapter6 {
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     // new range 0... Int.maxValue
     // old range Int.MinValue... Int.maxValue
-    //Int.MinValue
-    //rng.nextInt
+    val (next, nextRng) = rng.nextInt
+    (next / 2) - Int.MinValue / 2 -> nextRng
   }
 
   // 6.2 Write a function to generate a Double between 0 and 1, not including 1.
   // Note: You can use Int.MaxValue to obtain the maximum positive integer value, and you
   // can use x.toDouble to convert an x: Int to a Double.
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (next, nextRng) = nonNegativeInt(rng)
+    next.toDouble / (Int.MaxValue + 1.0) -> nextRng
+  }
 
   // 6.3 Write functions to generate an (Int, Double) pair, a (Double, Int) pair, and a
   // (Double, Double, Double) 3-tuple. You should be able to reuse the functions you’ve already written.
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (next, nextRng) = nonNegativeInt(rng)
+    val (next2, nextRng2) = double(nextRng)
+    (next -> next2) -> nextRng2
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (next, nextRng) = intDouble(rng)
+    next.swap -> nextRng
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    (d1, d2, d3) -> r3
+  }
 
   // 6.4 Write a function to generate a list of random integers.
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) {
+      Nil -> rng
+    } else {
+      val (next, nextRng) = nonNegativeInt(rng)
+      val (intss, finalRng) = ints(count - 1)(nextRng)
+      (next :: intss) -> finalRng
+    }
+  }
 
 
   type Rand[+A] = RNG => (A, RNG)
@@ -47,7 +72,7 @@ object Chapter6 {
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
     val (a, rng2) = s(rng)
     (f(a), rng2)
   }
@@ -61,13 +86,13 @@ object Chapter6 {
   // 6.6  Write the implementation of map2 based on the following signature.
   // This function takes two actions, ra and rb, and a function f for combining their results,
   // and returns a new action that combines them:
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
 
-  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
 
-  val randIntDouble: Rand[(Int, Double)] = both(int, double)
-  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+  lazy val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  lazy val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
 
   // 6.7 Hard: If you can combine two RNG transitions, you should be able to combine a whole list of them.
@@ -80,13 +105,13 @@ object Chapter6 {
 
   def nonNegativeLessThan(n: Int): Rand[Int] = { rng => val (i, rng2) = nonNegativeInt(rng)
     val mod = i % n
-    if (i + (n-1) - mod >= 0)
+    if (i + (n - 1) - mod >= 0)
       (mod, rng2)
     else nonNegativeLessThan(n)(rng)
   }
 
   // 6.8 Implement flatMap, and then use it to implement nonNegativeLessThan.
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 
   // 6.9 Reimplement map and map2 in terms of flatMap. The fact that this is possible is what
   // we’re referring to when we say that flatMap is more powerful than map and map2.
@@ -94,4 +119,7 @@ object Chapter6 {
 
   // TODO add missing exercices
 
+  def main(args: Array[String]) {
+    println(s"ints: ${ints(5)(new SimpleRNG(42))}")
+  }
 }
